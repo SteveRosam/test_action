7e2a2760c56695647f87084f3a953d2eae8d5bfd
SteveRosam <1799158+srosam@users.noreply.github.com>
2023-02-03 13:40:05 +0000

    log multi line

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
 
+messages = []
 
-def main():
+def send_log():
     env_file = os.getenv('GITHUB_ENV')
 
-    with open(env_file, "a") as myfile:
-        myfile.write("MY_VAR=MY_VALUE")
+    message = "\n".join(messages)
 
     with open(env_file, "a") as myfile:
-        myfile.write("\nMY_VAR=MY_VALUE2")
+        myfile.write(f"MY_VAR={message}")
+
+def log_message(message):
+    messages.append(message)
+
+
+def main():
+    try:
+        log_message("foo")
+        log_message("bar")
+        log_message("baz")
+    except Exception as e:
+        log_message("Error")
+    finally:
+        send_log()
 
 
 if __name__ == "__main__":
